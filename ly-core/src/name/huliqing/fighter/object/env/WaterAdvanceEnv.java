6a9cf797483ab7886c95a33ee48bd4128bdb0cfd
huliqing <huliqing@huliqing.name>
2016-07-09 01:06:30 +0800

    WaterAdvance连接xml; xml配置调整

--- ly-core/src/name/huliqing/fighter/object/env/WaterAdvanceEnv.java
+++ ly-core/src/name/huliqing/fighter/object/env/WaterAdvanceEnv.java
@@ -6,23 +6,14 @@
 package name.huliqing.fighter.object.env;
 
 import com.jme3.app.Application;
-import com.jme3.asset.AssetManager;
-import com.jme3.audio.AudioData.DataType;
-import com.jme3.audio.AudioNode;
-import com.jme3.audio.LowPassFilter;
 import com.jme3.light.DirectionalLight;
 import com.jme3.light.Light;
 import com.jme3.light.LightList;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
-import com.jme3.renderer.ViewPort;
-import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.control.AbstractControl;
 import com.jme3.texture.Texture2D;
 import com.jme3.water.WaterFilter;
+import com.jme3.water.WaterFilter.AreaShape;
 import name.huliqing.fighter.data.EnvData;
 import name.huliqing.fighter.object.scene.Scene;
 
@@ -33,80 +24,104 @@ import name.huliqing.fighter.object.scene.Scene;
  */
 public class WaterAdvanceEnv <T extends EnvData> extends Env<T> implements Scene.Listener {
 
-    private Application app;
-    private WaterFilter water;
-    private final LowPassFilter underWaterAudioFilter = new LowPassFilter(0.5f, 0.1f);
-//    private final LowPassFilter underWaterReverbFilter = new LowPassFilter(0.5f, 0.1f);
-    private final LowPassFilter aboveWaterAudioFilter = new LowPassFilter(1, 1);
-    private Node controlNode;
-    private AudioNode waveAudio;
+    private String causticsTexture;
+    private String foamTexture;
+    private String heightTexture;
+    private String normalTexture;
+    
+    // 是否使用场景中的直射光源，打开这个选项后Water会从场景中找到第一个可用的直射光源作为水体渲染时使用的光源。
+    // 否则使用Water默认设置的光源。默认true
+    private boolean useSceneLight = true;
+    
+    // ---- inner
+    private final WaterFilter water = new WaterFilter();
     
     @Override
     public void initData(T data) {
         super.initData(data); 
+        this.causticsTexture = data.getAttribute("causticsTexture");
+        this.foamTexture = data.getAttribute("foamTexture");
+        this.heightTexture = data.getAttribute("heightTexture");
+        this.normalTexture = data.getAttribute("normalTexture");
+        water.setCausticsIntensity(data.getAsFloat("causticsIntensity", water.getCausticsIntensity()));
+        Vector3f center = data.getAsVector3f("center");
+        if (center != null) {
+            water.setCenter(center);
+        }
+        water.setColorExtinction(data.getAsVector3f("colorExtinction", water.getColorExtinction()));
+        water.setDeepWaterColor(data.getAsColor("deepWaterColor", water.getDeepWaterColor()));
+        water.setFoamExistence(data.getAsVector3f("foamExistence", water.getFoamExistence()));
+        water.setFoamHardness(data.getAsFloat("foamHardness", water.getFoamHardness()));
+        water.setFoamIntensity(data.getAsFloat("foamIntensity", water.getFoamIntensity()));
+        water.setLightColor(data.getAsColor("lightColor", water.getLightColor()));
+        water.setLightDirection(data.getAsVector3f("lightDirection", water.getLightDirection()));
+        water.setMaxAmplitude(data.getAsFloat("maxAmplitude", water.getMaxAmplitude()));
+        water.setNormalScale(data.getAsFloat("normalScale", water.getNormalScale()));
+        water.setRadius(data.getAsFloat("radius", water.getRadius()));
+        water.setReflectionDisplace(data.getAsFloat("reflectionDisplace", water.getReflectionDisplace()));
+        water.setReflectionMapSize(data.getAsInteger("reflectionMapSize", water.getReflectionMapSize()));
+//        water.setReflectionScene(controlNode);
+        water.setRefractionConstant(data.getAsFloat("refractionConstant", water.getRefractionConstant()));
+        water.setRefractionStrength(data.getAsFloat("refractionStrength", water.getRefractionStrength()));
+        
+        String shapeType = data.getAttribute("shapeType");
+        if (AreaShape.Square.name().equals(shapeType)) {
+            water.setShapeType(AreaShape.Square);
+        } else if (AreaShape.Circular.name().equals(shapeType)) {
+            water.setShapeType(AreaShape.Circular);
+        }
+        
+        water.setShininess(data.getAsFloat("shininess", water.getShininess()));
+        water.setShoreHardness(data.getAsFloat("shoreHardness", water.getShoreHardness()));
+        water.setSpeed(data.getAsFloat("speed", water.getSpeed()));
+        water.setSunScale(data.getAsFloat("sunScale", water.getSunScale()));
+        water.setUnderWaterFogDistance(data.getAsFloat("underWaterFogDistance", water.getUnderWaterFogDistance()));
+        water.setUseCaustics(data.getAsBoolean("useCaustics", water.isUseCaustics()));
+        water.setUseFoam(data.getAsBoolean("useFoam", water.isUseFoam()));
+        water.setUseHQShoreline(data.getAsBoolean("useHQShoreline", water.isUseHQShoreline()));
+        water.setUseRefraction(data.getAsBoolean("useRefraction", water.isUseRefraction()));
+        water.setUseRipples(data.getAsBoolean("useRipples", water.isUseRipples()));
+        water.setUseSpecular(data.getAsBoolean("useSpecular", water.isUseSpecular()));
+        water.setWaterColor(data.getAsColor("waterColor", water.getWaterColor()));
+        water.setWaterHeight(data.getAsFloat("waterHeight", water.getWaterHeight()));
+        water.setWaterTransparency(data.getAsFloat("waterTransparency", water.getWaterTransparency()));
+        water.setWaveScale(data.getAsFloat("waveScale", water.getWaveScale()));
+        water.setWindDirection(data.getAsVector2f("windDirection", water.getWindDirection()));
+        
+        // ---- custom 
+        useSceneLight = data.getAsBoolean("useSceneLight", useSceneLight);
     }
 
     @Override
     public void initialize(Application app, Scene scene) {
         super.initialize(app, scene); 
-        this.app = app;
         scene.addListener(this);
         
-         //Water Filter
-        water = new WaterFilter();
-        water.setWaterColor(new ColorRGBA(0.6f, 0.8f, 0.9f, 1));
-        water.setDeepWaterColor(new ColorRGBA(0.6f, 0.8f, 0.9f, 1));
-        water.setUnderWaterFogDistance(80);
-        water.setWaterTransparency(0.12f);
-        water.setFoamIntensity(0.4f);        
-        water.setFoamHardness(0.3f);
-        water.setFoamExistence(new Vector3f(0.8f, 8f, 1f));
-        water.setReflectionDisplace(50);
-        water.setRefractionConstant(0.25f);
-        water.setColorExtinction(new Vector3f(30, 50, 70));
-        water.setCausticsIntensity(0.4f);        
-        water.setWaveScale(0.003f);
-        water.setMaxAmplitude(2f);
-        water.setFoamTexture((Texture2D) app.getAssetManager().loadTexture("Common/MatDefs/Water/Textures/foam2.jpg"));
-        water.setRefractionStrength(0.2f);
+        if (causticsTexture != null) {
+            water.setCausticsTexture((Texture2D) app.getAssetManager().loadTexture(causticsTexture));
+        }
+        if (foamTexture != null) {
+            water.setFoamTexture((Texture2D) app.getAssetManager().loadTexture(foamTexture));
+        }
+        if (heightTexture != null) {
+            water.setHeightTexture((Texture2D) app.getAssetManager().loadTexture(heightTexture));
+        }
+        if (normalTexture != null) {
+            water.setNormalTexture((Texture2D) app.getAssetManager().loadTexture(normalTexture));
+        }
         water.setReflectionScene(scene.getSceneRoot());
-        
-        controlNode = new Node();
-        controlNode.addControl(new WaterControl());
-        
-        waveAudio = createWaveAudio(app.getAssetManager());
         
         // 控制水体渲染
         scene.addFilter(water);
-        // 控制潮涨潮落
-        scene.addSceneObject(controlNode);
-        // 控制潮水声效
-        scene.addSceneObject(waveAudio);
-        waveAudio.play();
     }
 
     @Override
     public void cleanup() {
-        if (scene != null) {
-            waveAudio.stop();
-            scene.removeListener(this);
-            scene.removeSceneObject(controlNode);
-            scene.removeSceneObject(waveAudio);
-            scene.removeFilter(water);
-            water = null;
-            controlNode = null;
-            waveAudio = null;
-        }
+        scene.removeListener(this);
+        scene.removeFilter(water);
         super.cleanup();
     }
     
-    private AudioNode createWaveAudio(AssetManager am) {
-        AudioNode audio = new AudioNode(am, "Sounds/environment/ocean.ogg", DataType.Buffer);
-        audio.setLooping(true);
-        audio.setReverbEnabled(true);
-        return audio;
-    }
-
     @Override
     public void onAdded(Scene scene, Spatial objectAdded) {}
 
@@ -115,48 +130,52 @@ public class WaterAdvanceEnv <T extends EnvData> extends Env<T> implements Scene
 
     @Override
     public void onInitialized(Scene scene) {
-        LightList lights = scene.getSceneRoot().getLocalLightList();
-        if (lights.size() > 0) {
-            for (int i = 0; i < lights.size(); i++) {
-                Light light = lights.get(i);
-                if (light instanceof DirectionalLight) {
-                    DirectionalLight dl = (DirectionalLight) light;
-                    water.setLightDirection(dl.getDirection());
-                    water.setLightColor(dl.getColor());
-                    break;
+        // 从场景中找到第一个直射光源作为水体渲染光源用。
+        if (useSceneLight) {
+            LightList lights = scene.getSceneRoot().getLocalLightList();
+            if (lights.size() > 0) {
+                for (int i = 0; i < lights.size(); i++) {
+                    Light light = lights.get(i);
+                    if (light instanceof DirectionalLight) {
+                        DirectionalLight dl = (DirectionalLight) light;
+                        water.setLightDirection(dl.getDirection());
+                        water.setLightColor(dl.getColor());
+                        break;
+                    }
                 }
-            }
+            }            
         }
     }
     
-    private class WaterControl extends AbstractControl {
-        //This part is to emulate tides, slightly varrying the height of the water plane
-        private float time = 0.0f;
-        private float waterHeight = 0.0f;
-        private float initialWaterHeight = -6.5f;//0.8f;
-        private boolean underWater = false;
-        
-        @Override
-        protected void controlUpdate(float tpf) {
-            time += tpf;
-            waterHeight = (float) Math.cos(((time * 0.6f) % FastMath.TWO_PI)) * 1.5f;
-            water.setWaterHeight(initialWaterHeight + waterHeight);
-            if (water.isUnderWater() && !underWater) {
-                waveAudio.setReverbEnabled(true);
-                waveAudio.setDryFilter(underWaterAudioFilter);
-                underWater = true;
-            }
-            if (!water.isUnderWater() && underWater) {
-                underWater = false;
-                waveAudio.setReverbEnabled(false);
-                waveAudio.setDryFilter(aboveWaterAudioFilter);
-            }
-        }
-
-        @Override
-        protected void controlRender(RenderManager rm, ViewPort vp) {}
-        
-    }
+    // 控制潮涨潮落
+//    private class WaterControl extends AbstractControl {
+//        //This part is to emulate tides, slightly varrying the height of the water plane
+//        private float time = 0.0f;
+//        private float waterHeight = 0.0f;
+//        private float initialWaterHeight = -6.5f;//0.8f;
+//        private boolean underWater = false;
+//        
+//        @Override
+//        protected void controlUpdate(float tpf) {
+//            time += tpf;
+//            waterHeight = (float) Math.cos(((time * 0.6f) % FastMath.TWO_PI)) * 1.5f;
+//            water.setWaterHeight(initialWaterHeight + waterHeight);
+//            if (water.isUnderWater() && !underWater) {
+//                waveAudio.setReverbEnabled(true);
+//                waveAudio.setDryFilter(underWaterAudioFilter);
+//                underWater = true;
+//            }
+//            if (!water.isUnderWater() && underWater) {
+//                underWater = false;
+//                waveAudio.setReverbEnabled(false);
+//                waveAudio.setDryFilter(aboveWaterAudioFilter);
+//            }
+//        }
+//
+//        @Override
+//        protected void controlRender(RenderManager rm, ViewPort vp) {}
+//        
+//    }
     
     
 }
