ab708476109d2319b4a282fafc56176e718c8806
huliqing <huliqing@huliqing.name>
2016-10-16 14:20:31 +0800

    new game project luoying3

---
+++ ly-luoying-desktop3/src/name/huliqing/luoying/game/Start.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name.huliqing.luoying.game;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ *
+ * @author huliqing
+ */
+public class Start extends SimpleApplication {
+    
+    public static void main(String[] args) {
+        Start s = new Start();
+        s.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+        
+    }
+    
+}
