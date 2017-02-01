2f80a68e620166af475eb1e71389cbad9d5f17a7
huliqing <huliqing@huliqing.name>
2017-02-01 15:47:04 +0800

    重构编辑控制工具

---
+++ ly-editor/src/name/huliqing/editor/edit/controls/ControlTile.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name.huliqing.editor.edit.controls;
+
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+/**
+ * 控制组件
+ * @author huliqing
+ */
+public interface ControlTile {
+    
+    void initialize();
+    
+    void cleanup();
+    
+    void setLocalTranslation(Vector3f location);
+    
+    void setLocalRotation(Quaternion rotation);
+    
+    void setLocalScale(Vector3f scale);
+    
+    /**
+     * 获取被选中的物体空间，该方法只作为只读使用,<b>不要</b>直接设置该物体的变换。
+     * @return 
+     */
+    Spatial getReadOnlySpatial();
+    
+    
+}
